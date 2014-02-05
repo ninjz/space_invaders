3a8c499189e4f8a779869de6ca6dde0a44ba5d51
Calvin <cchengtt@gmail.com>
2014-02-05 16:56:39 -0500

    crazy cocaine party level.

--- src/js/spaceInvaders.js
+++ src/js/spaceInvaders.js
@@ -1,14 +1,18 @@
 var canvas, context;
-var alien1, alien2, alien3, cannon, bullet, logo;
+var alien1, alien2, alien3, cannon, bullet, logo, bomb;
+var alien1_1, alien2_1, alien3_1;
+var images = [];
 var invaderArray = [];
 var alien1Array = [];
 var alien2Array = [];
 var alien3Array = [];
 var projectiles = [];
+var bombs = [];
 var player;
 var switchFrame = false;
 var lives = 2;
 var score = 0;
+var moveState = 0; // 0: right, 1: left, 2: down
 
 
 // ************************************************************************ 
@@ -22,23 +26,31 @@ function init(){
 	alien1_1 = new Image();
 
 	alien2 = new Image();
-	// alien2_1 = newImage();
+	alien2_1 = new Image();
 
 	alien3 = new Image();
-	// alien2_1 = newImage();
-	cannon = new Image();
+	alien3_1 = new Image();
 	bullet = new Image();
 	logo = new Image();
+	bomb = new Image();
+
+	cannon = new Image();
+	
 
 	alien1.src = "alien01_01.png";
 	alien1_1.src = "alien01_02.png";
+	alien2_1.src = "alien02_02.png";
+	alien3_1.src = "alien03_02.png";
 
 	alien2.src = "alien02_01.png";
 	alien3.src = "alien03_01.png";
-	cannon.src = "cannon.png";
 	bullet.src = "bullet.png";
 
 	logo.src = "logo.png";
+	bomb.src = "alien_bomb.png";
+	cannon.src = "cannon.png";
+	
+	images = [alien1,alien2,alien3, alien1_1, alien2_1, alien3_1, cannon, bullet, logo];
 
 	canvas = document.getElementById("spaceInvaders");
 	context = canvas.getContext("2d");
@@ -114,6 +126,7 @@ function loadAssets(){
 // ************************************************************************ 
 // Invader Class Object
 // *********************************************************************** 
+var invaderSpeed = 0.5;
 function Invader(type, x, y){
 	this.constructor.population++;
 	var alive=true;
@@ -122,25 +135,52 @@ function Invader(type, x, y){
 	this.y = y;
 
 	this.moveDown=function(){
-		this.y++;
+		this.y += invaderSpeed;
 	};
 
 	this.moveLeft=function(){
-		this.x--;
+		this.x -= invaderSpeed;
 	};
 
 	this.moveRight=function(){
-		this.x++;
+		this.x += invaderSpeed;
 	};
 
 	this.draw=function(){
 		// context.drawImage(this.type, this.x, this.y);
-		if(switchFrame){
-			context.drawImage(alien1, this.x, this.y);
-		} else {
-			context.drawImage(alien1_1, this.x , this.y);
+		switch(this.type)
+		{
+			case alien1:
+				if(switchFrame){
+					context.drawImage(alien1, this.x, this.y);
+				} else {
+					context.drawImage(alien1_1, this.x , this.y);
+				}
+				break;
+			case alien2:
+				if(switchFrame){
+					context.drawImage(alien2, this.x, this.y);
+				} else {
+					context.drawImage(alien2_1, this.x , this.y);
+				}
+				break;
+			case alien3:
+				if(switchFrame){
+					context.drawImage(alien3, this.x, this.y);
+				} else {
+					context.drawImage(alien3_1, this.x , this.y);
+				}
+				break;
+			default:
+				break;
+
 		}
 		
+		
+	};
+
+	this.shoot=function(){
+		bombs.push(new Bomb(this.x + 11, this.y+10));
 	};
 
 	this.testHit = function(x , y){
@@ -241,10 +281,34 @@ function Projectile(x, y){
 					active = false;
 				}
 			}
-		}
-		
+		}	
 	};
+}
 
+// ************************************************************************ 
+// Bomb Class Object
+// *********************************************************************** 
+function Bomb(x, y){
+
+	var active = true;
+	this.x = x;
+	this.y = y;
+
+	this.draw=function(){
+		context.drawImage(bomb, this.x, this.y += 5);
+		if(this.y > 0){
+			//deallocate self
+		}
+		if(active){
+			if( Math.abs(this.x - player.x) < 30){
+				lives--;
+				// restart game?
+				if(lives == 0){
+					// GAME OVER
+				}
+			}
+		}	
+	};
 }
 
 
@@ -290,7 +354,13 @@ function draw(){
 		projectiles[i].draw();
 	}
 
+	for(i = 0; i < bombs.length; i++){
+		bombs[i].draw();
+	}
+
 }
+
+
 
 function displayMenu(){
 	context.clearRect(0, 0, canvas.width, canvas.height);
@@ -302,28 +372,81 @@ function displayMenu(){
 	context.font="20px Georgia";
 	context.fillStyle="white";
 
-	context.fillText("Press [TAB} to start!",110,300);
+	context.fillText("Press [TAB] to start!",110,300);
 	window.addEventListener('keydown', handleInput, true);
 
 }
 
+
 function tick(){
 	// HUD 
+	var colDecision;
+	var rowDecision = Math.floor((Math.random()*3)+1);  // random number between 1 and 3.
+	switch(rowDecision){
+		case 1:
+			alien1Array[Math.floor((Math.random()*(alien1Array.length - 1)) + 0)].shoot();
+			break;
+		case 2:
+			alien2Array[Math.floor((Math.random()*(alien2Array.length - 1)) + 0)].shoot();
+			break;
+		case 3:
+			alien3Array[Math.floor((Math.random()*(alien2Array.length - 1)) + 0)].shoot();
+			break;
+	}
 
-	// move aliens
-	// if(){
-		// for(var i = 0; i < alien1Array.length; i++){
-		// 	alien1Array[i].moveDown();
-		// }
 
-		// for(i = 0; i < alien2Array.length; i++){
-		// 	alien2Array[i].moveDown();
-		// }
+	switch(moveState){
+		case 0:
+			for(var i = 0; i < alien1Array.length; i++){
+				alien1Array[i].moveRight();
+			}
 
-		// for(i = 0; i < alien3Array.length; i++){
-		// 	alien3Array[i].moveDown();
-		// }
-	// }
+			for(i = 0; i < alien2Array.length; i++){
+				alien2Array[i].moveRight();
+			}
+
+			for(i = 0; i < alien3Array.length; i++){
+				alien3Array[i].moveRight();
+			}
+
+			// if(alien3Array[alien3Array.length - 1].x == 350){
+			// 	startMove = 1;
+			// }
+			break;
+		case 1:
+			for(var i = 0; i < alien1Array.length; i++){
+				alien1Array[i].moveLeft();
+			}
+
+			for(i = 0; i < alien2Array.length; i++){
+				alien2Array[i].moveLeft();
+			}
+
+			for(i = 0; i < alien3Array.length; i++){
+				alien3Array[i].moveLeft();
+			}
+			// if(alien3Array[0].x <= 10){
+			// 	startMove = 0;
+			// }
+
+			break;
+		case 2:
+			for(var i = 0; i < alien1Array.length; i++){
+				alien1Array[i].moveDown();
+			}
+
+			for(i = 0; i < alien2Array.length; i++){
+				alien2Array[i].moveDown();
+			}
+
+			for(i = 0; i < alien3Array.length; i++){
+				alien3Array[i].moveDown();
+			}
+
+			break;
+
+	}
+	
 	
 
 	// randomly select invader to drop bomb
@@ -333,8 +456,33 @@ function tick(){
 	
 }
 
+var startX = 0;
 function setFrame(){
 	switchFrame = !switchFrame;
+	// move aliens
+	startX++;
+	console.log(startX);
+	if(startX >= 10){
+		moveState = 2;
+	} 
+
+	if(startX >= 11){
+		moveState = 1;
+	}
+
+	if(startX >= 21){
+		moveState = 2;
+	}
+
+	if(startX >= 22){
+		moveState = 0;
+		startX = 0;
+	}
+
+
+
+	
+
 	setTimeout(function(){setFrame();}, 300);
 }
 
